4da30f71121829e5803feeccf85e8c74b2a313a4
ChiRuiChen <kugelblitz.ee05@gmail.com>
2023-01-16 14:15:14 +0800

    upload

---
+++ dcalo_img_AP_12_3/project.tcl
@@ -0,0 +1,8 @@
+variable project_name
+set project_name "myproject"
+variable backend
+set backend "vivado"
+variable part
+set part "xczu9eg-ffvb1156-2-e"
+variable clock_period
+set clock_period 5
